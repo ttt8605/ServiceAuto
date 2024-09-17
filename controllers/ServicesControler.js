b823a1e43a776405d173a70c2b724325166b67d7
ttt8605 <64919222+ttt8605@users.noreply.github.com>
2024-09-17 11:58:12 +0300

    Small adjustemnts and statuspage started

--- controllers/ServicesControler.js
+++ controllers/ServicesControler.js
@@ -36,13 +36,12 @@ const formatDescription = (text) => {
     // Trim the text to remove leading/trailing whitespace and ensure there's no newline at the end
     return text
       .trim()
-      .replace(/\n+$/, '') // Remove any trailing newlines
-      .replace(/\n/g, '<br>') // Replace remaining newlines with <br>
+      .replace(/\n{2,}/g, '<br><br>') // Replace multiple newlines (2 or more) with <br><br> for paragraph breaks
+      .replace(/\n/g, '<br>') // Replace single newlines with <br>
       .replace(/ {4}/g, ' &nbsp;&nbsp;&nbsp;&nbsp;') // Replace quadruple spaces with &nbsp;&nbsp;&nbsp;&nbsp;
       .replace(/ {3}/g, ' &nbsp;&nbsp;&nbsp;') // Replace triple spaces with &nbsp;&nbsp;&nbsp;
       .replace(/ {2}/g, ' &nbsp;'); // Replace double spaces with &nbsp;
-  };
-
+};
 
 module.exports.ServiceIndividual = async (req, res) => {
     
