43b8bb36ace3c3b3b2d8449ebb3db6e879cd0961
ttt8605 <64919222+ttt8605@users.noreply.github.com>
2024-09-19 12:07:26 +0300

    pagina programari staff done

--- middleware.js
+++ middleware.js
@@ -1,7 +1,7 @@
 module.exports.isLoggedIn = (req,res,next)=>{
     if(!req.isAuthenticated()){
         req.flash('error','You are not authorized');
-        return res.redirect('/drones')
+        return res.redirect('/*')
     }
     next();
     }
